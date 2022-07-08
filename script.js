f2ebbf2e3e43b44673bd5b13f601dc844e405de4
Arturo <arturo9503@gmail.com>
2022-07-08 15:54:07 -0400

    Add mouse-hover highlight function

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ for (let i = 0 ; i < 256; i++){
     const div = document.createElement('div');
     div.style.cssText += 'width: 6.25%';
     div.style.cssText += 'height: 6.25vh';
-    div.style.cssText += 'background-color: red';
+    div.addEventListener("mouseover", function(e){
+        e.target.style.cssText += 'background-color: yellow';
+        setTimeout(function(){
+            e.target.style.backgroundColor = ""; 
+        }, 500);
+    }, false);
     container.appendChild(div);
 }
+
+
 
 
