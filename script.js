0745933f0376b5e14a103fde2b8d6d9e723ff0b0
Arturo <arturo9503@gmail.com>
2022-07-09 12:00:45 -0400

    Add buttons for dynamic grid sizing

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const container = document.querySelector("#container");
-container.style.cssText += 'display: flex';
-container.style.cssText += 'flex-wrap: wrap';
+container.style.cssText = 'display: flex; flex-wrap: wrap';
 
-for (let i = 0 ; i < 256; i++){
-    const div = document.createElement('div');
-    div.style.cssText += 'width: 6.25%';
-    div.style.cssText += 'height: 6.25vh';
-    div.addEventListener("mouseover", function(e){
-        e.target.style.cssText += 'background-color: yellow';
-        setTimeout(function(){
-            e.target.style.backgroundColor = ""; 
-        }, 500);
-    }, false);
-    container.appendChild(div);
+const header = document.createElement("div");
+header.textContent ="Number of pixels (n x n): ";
+header.style.cssText = 'display: flex; gap: 15px; margin: 15px; width: 100% ; padding: 15px ;align-items: center ; justify-content: center;'
+
+
+for (let i = 25 ; i <= 100 ; i += 25){
+    const btn = document.createElement('button');
+    btn.textContent= i;
+    btn.addEventListener('click', function(e){
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.remove();
+        });
+        n = this.textContent;
+        createGrid(n);
+    });
+    header.appendChild(btn);
 }
+
+container.append(header);
+
+
+
+function createGrid(n){
+    for (let i = 0 ; i < n*n; i++){
+        const div = document.createElement('div');
+        div.classList.add('square');
+        div.style['width'] = 1/n*100 + "%";
+        div.style['height'] = 1/n*100 + "vh";
+        div.addEventListener("mouseover", function(e){
+            e.target.style.cssText += 'background-color: blue';
+        });
+        container.appendChild(div);
+    }
+}
+
 
 
 
